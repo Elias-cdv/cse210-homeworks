4365b2d8fc41e68725c29a15bfd19baaeb5cecb7
Elías Cabeza de Vaca <elways38@gmail.com>
2026-01-07 12:14:40 -0500

    añadiendo archivos de prueba de c#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> planViernes = new List<string>();
+
+planViernes.Add("Ir al templo a las 7am");
+planViernes.Add("Esperar en la puerta");
+planViernes.Add("Ver a Raquel llegar");
+planViernes.Add("Abrazarla");
+
+Console.WriteLine("Plan para el viernes: ");
+foreach (var tarea in planViernes)
+{
+    Console.WriteLine($"-{tarea}");
+}
